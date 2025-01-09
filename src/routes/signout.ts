eabd23a67e07637960c8b7c7be166d6dd97bb7c9
sanjib-12 <sanjibbarakoti112@gmail.com>
2025-01-09 18:18:01 -0500

    added the signout function

--- src/routes/signout.ts
+++ src/routes/signout.ts
@@ -2,7 +2,8 @@ import express from "express";
 const router = express.Router();
 
 router.get('/api/users/signout', (req, res) =>{
-   res.send('Hi there!');
+   req.session = null;
+   res.send({});
 });
 
 export {router as signoutRouter};
