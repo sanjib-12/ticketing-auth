3ccc19b0f39f2e170b716ab67e1d4596f0108b4c
sanjib-12 <sanjibbarakoti112@gmail.com>
2025-01-11 00:08:09 -0500

    added test for current-user and signout apis

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -5,7 +5,7 @@ import { currentUSer } from "../middlewares/current-user";
 const router = express.Router();
 
 router.get("/api/users/currentuser", currentUSer, (req, res) => {
-   res.send({ currentUSer: req.currentUser || null });
+   res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
