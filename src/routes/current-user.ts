4849fc314211025410b1cef4094aa77cb78c145b
sanjib-12 <sanjibbarakoti112@gmail.com>
2025-01-21 20:27:00 -0500

    Added the custom library removing reusable codes

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { currentUSer } from "../middlewares/current-user";
+import { currentUSer } from "@sbmytickets/common";
 
 const router = express.Router();
 
